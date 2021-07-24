b42e11f779c6bd28bbc42aaaabb7cc5b248221b1
Arthur <arthurnarimanov@gmail.com>
2021-07-24 11:28:23 +0300

    Init project

---
+++ SearchMovies/SearchMovies/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  SearchMovies
+//
+//  Created by Arthur Narimanov on 7/24/21.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+	var window: UIWindow?
+
+	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+		let vc = ViewController()
+		let frame = UIScreen.main.bounds
+		
+		window = UIWindow(frame: frame)
+		window?.makeKeyAndVisible()
+		window?.rootViewController = vc
+		return true
+	}
+
+}
+
---
+++ SearchMovies/SearchMovies/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  SearchMovies
+//
+//  Created by Arthur Narimanov on 7/24/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		view.backgroundColor = .systemYellow
+	}
+	
+}
+
